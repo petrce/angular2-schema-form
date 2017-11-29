0304596a6ddadd79296b3409fd6f19cde310fa2f
Frantisek Skorunka <skorunka@outlook.com>
2017-11-29 14:48:24 +0000

    ng5 update

--- tests/src/app/app.module.ts
+++ tests/src/app/app.module.ts
@@ -1,18 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { SchemaFormModule } from './lib';
 import { AppComponent } from './app.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [BrowserModule, SchemaFormModule],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
